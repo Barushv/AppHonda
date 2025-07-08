a2aefbbac95e9561aa02b5a4c8e50e93c53d62bc
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2025-07-08 13:13:58 -0600

    Mejorando el menu
    Se agrega mejoras en menu e iconos

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - CACHE DE PWA
 // =====================================
 
-const CACHE_NAME = "honda-app-cache";
+const CACHE_NAME = "honda-app-cache-icons";
 const urlsToCache = [
   "index.html",
   "styles.css",
