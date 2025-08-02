7b1fd4e20f82b8c87e6135b49a580017acdd06fe
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2025-08-02 14:25:25 -0600

    Update service-worker.js
    Actualizacion para oferta comercial de Agosto

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - CACHE DE PWA
 // =====================================
 
-const CACHE_NAME = "honda-app-cache-calendario";
+const CACHE_NAME = "honda-app-cache-agosto";
 const urlsToCache = [
   "index.html",
   "css/styles.css",
@@ -85,3 +85,4 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
+
