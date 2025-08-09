b1da35945ce24f6fbba24579682419e80183d840
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2025-08-09 14:58:38 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER - CACHE DE PWA
 // =====================================
 
-const CACHE_NAME = "honda-app-cache-cliente";
+const CACHE_NAME = "honda-app-cache-clienteUI";
 const urlsToCache = [
   "index.html",
   "css/styles.css",
@@ -85,4 +85,5 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
+
 
