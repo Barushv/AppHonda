4dccc4a5b72d9485db7fa1c0344dcefc37a17ad0
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2026-01-03 15:04:43 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =====================================
 
 // ¡CAMBIA este nombre cada vez que publiques cambios importantes!
-const CACHE_NAME = "hondago-december2";
+const CACHE_NAME = "hondago-enero2026";
 
 // Archivos base a precachear
 const urlsToCache = [
