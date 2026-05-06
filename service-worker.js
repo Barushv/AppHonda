40b402c301e7d7301dc9a9b86ec989ee446a2352
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2026-05-06 13:25:46 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =====================================
 
 // ¡CAMBIA este nombre cada vez que publiques cambios importantes!
-const CACHE_NAME = "HondaGo-v2.1.1";
+const CACHE_NAME = "hondago-enero2026";
 
 // Archivos base a precachear
 const urlsToCache = ["index.html", "css/styles.css", "js/app.js", "img/logo-honda.png", "img/icono-pwa.png", "img/guardias.png", "json/precios.json"];
